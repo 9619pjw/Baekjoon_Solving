d58e4ed8478b470a87f977f0b6a34a404eee8ec0
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2022-12-05 23:51:02 +0900

    [Bronze V] Title: 사파리월드, Time: 212 ms, Memory: 17836 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		if((a.compareTo(b)>0))
+			System.out.println(a.subtract(b));
+		else
+			System.out.println(b.subtract(a));
+			
+		
+	}
+}
