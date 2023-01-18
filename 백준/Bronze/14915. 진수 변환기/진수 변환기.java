eabbbdfebb041464cc1a6a06349a426b9e63b551
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-01-18 12:36:25 +0900

    [Bronze II] Title: 진수 변환기, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/14915. 진수 변환기/진수 변환기.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();  // 입력받은 수
+		int b = sc.nextInt(); //  n진수
+		
+		Stack st= new Stack();
+        if(a == 0) System.out.println(0);
+		//나머지값을 스택에 저장 후 나머지값을 스택에서 꺼내어 출력
+		while(a != 0){
+			int tmp = a%b;
+			if(tmp < 10) // 나머지가 10보다 작으면 
+				st.push(tmp); // 스택에 나머지값 저장
+			else{			// 나머지가 10보다 큰 경우 변환시킴
+				switch(tmp){
+					case 10:
+						st.push('A');
+						break;
+					case 11:
+						st.push('B');
+						break;
+					case 12:
+						st.push('C');
+						break;
+					case 13:
+						st.push('D');
+						break;
+					case 14:
+						st.push('E');
+						break;
+					case 15:
+						st.push('F');
+						break;
+				}
+			}	
+			a /= b; // 계속 나눠줌
+		}
+		while(!st.isEmpty()){
+			System.out.print(st.pop());			// 스택의 내용물을 전부 꺼냄
+		}
+	}
+}
