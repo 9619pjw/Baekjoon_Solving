e339bef28a64595036aff57f5011062a63af7b2a
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-01-19 12:56:24 +0900

    Update 단어 공부.java

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,5 +1,42 @@
 import java.util.Scanner;
 
+class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		int a[] = new int[26]; // 알파벳 사용횟수 저장하는 배열
+		
+        for(int i = 0 ; i<s.length(); i++){
+			if(s.charAt(i) >= 97 )  // 소문자 입력일 때
+				a[s.charAt(i) - 97]++;
+			else                    // 대문자 입력일 때
+				a[s.charAt(i) - 65]++;
+		}
+        int max = 0; // 가장 많이 나온 알파벳의 개수 저장됨
+		int cnt = 0; // max의 개수, 1 이상일 때는 "?" 출력하게 만든다.
+		for(int i =0; i<a.length; i++){     // 배열에서 가장 많이 나온 알파벳의 개수를 max에 저장
+			if(max < a[i])
+				max = a[i];
+		}
+
+		for(int i = 0; i<a.length; i++){
+			if(max == a[i])             // max와 알파벳의 개수가 같으면 1증가
+				cnt++;  
+		}
+
+		for(int i =0; i<a.length; i++){
+			if(max == a[i] && cnt == 1)
+				System.out.println((char)(i+'A'));
+			else if (max == a[i] && cnt > 1){
+				System.out.println("?");
+				break;
+			}
+		}
+	}
+}
+/*
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -32,3 +69,4 @@ public class Main{
         }
     }
 }
+*/
