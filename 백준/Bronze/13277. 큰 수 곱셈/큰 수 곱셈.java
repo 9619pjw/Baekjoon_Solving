9d1e8e4a80e720e085fce98e16bbd2d8b410ffbd
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-01-19 14:27:52 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6244 ms, Memory: 398524 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		BigInteger c = b.multiply(a);
+
+		System.out.println(c);
+	}
+}
