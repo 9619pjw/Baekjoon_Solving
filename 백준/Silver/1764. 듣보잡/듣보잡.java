2179a3d237046c16b828dcddf4bff86db6ea537c
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-01-27 17:30:06 +0900

    [Silver IV] Title: 듣보잡, Time: 772 ms, Memory: 70176 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+
 
 class Main{
     public static void main(String[] args){
@@ -11,21 +12,22 @@ class Main{
         
         int cnt = 0; // 중복되는 사람의 수
 
-        ArrayList<String> noHearSee = new ArrayList<>(); // 듣도보도 못한 사람 하나의 배열 리스트에 담음
+        String noHearSee[] = new String[N+M]; // 듣도보도 못한 사람 하나의 배열 리스트에 담음
         ArrayList<String> no = new ArrayList<>(); // 중복되는 사람을 담는 배열 리스트
 
         for(int i =0; i<N+M; i++){
-            noHearSee.add(sc.next());   // 한번에 입력받음
+            noHearSee[i] = sc.next();   // 한번에 입력받음
         }
 
-        Collections.sort(noHearSee); // 입력받은 사람들을 정렬함
+        Arrays.sort(noHearSee); // 입력받은 사람들 정렬함
+
+        
         for(int i=0; i<N+M-1; i++){// 정렬된 배열 요소를 비교하여 같은 이름이 있나 검사함
-            if(noHearSee.get(i).equals(noHearSee.get(i+1))){ 
-                cnt++; 
-                no.add(noHearSee.get(i)); // 중복 횟수 증가 및 no 배열리스트에 추가함
-            }
+           if(noHearSee[i].equals(noHearSee[i+1])){
+                cnt++;
+                no.add(noHearSee[i]);
+           }
         }
-
         System.out.println(cnt); // 중복 횟수 출력
         for(int i =0; i<no.size(); i++){
             System.out.println(no.get(i));
