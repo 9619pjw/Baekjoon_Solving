c70e895f62ed8a2adeec1d07bf61fdc3f7501eff
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-02-19 10:47:08 +0900

    [Bronze III] Title: 세 수, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int arr[] = new int[3];
+
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i =0; i<arr.length-1; i++){
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(arr[1]);
+        br.close();
+    }
+}
