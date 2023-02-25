e39d42bd3da8158d23f22906f54d5c3a8902787c
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-02-25 21:17:25 +0900

    [Silver IV] Title: 덱, Time: 380 ms, Memory: 20056 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -2,9 +2,6 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    public static void Dequeue(){
-
-    }
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
