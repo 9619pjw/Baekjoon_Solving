5ec622787b5fff07bc45c97040ed8b3bebb9d9ae
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-02-28 08:57:18 +0900

    [Silver V] Title: 수들의 합 5, Time: 192 ms, Memory: 53368 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -8,11 +8,8 @@ class Main{
         int arr[] = new int[N];
         int cnt = 1; // N이 입력한 숫자 하나만 뽑을 경우의 수를 미리 넣음 
         int sum = 1;
-        for(int i =0; i<N; i++){
-            arr[i] = i+1; // 배열에 1부터 N까지 숫자 저장
-        }
-        int start_index = arr[0];
-        int end_index = arr[0];
+        int start_index = 1;
+        int end_index = 1;
 
         while(end_index != N){
             if(sum == N){
