e91bfb5239b5cde3594dce373a799ecd3c3ac6df
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-03-14 22:00:10 +0900

    [Silver I 11660] Title: 구간 합 구하기 5, Time: 1724 ms, Memory: 128000 KB -NKLCBHub

---
+++ 백준/Silver/#11660 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        } // 2차원 배열 저장
+
+        int D[][] = new int[N + 1][N + 1]; // 구간 합 저장할 배열 생성
+
+        for (int i = 1; i <= N; i++) { // 구간 합 배열
+            for (int j = 1; j <= N; j++) {
+                D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < M; i++) { // 구간 합 출력
+            st = new StringTokenizer(br.readLine(), " "); // 좌표값 입력받아서 출력
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+            System.out.println(result);
+        }
+        br.close();
+    }
+}
