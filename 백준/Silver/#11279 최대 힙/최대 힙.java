a4c138cdedda7ff9b872234346905b0c6816f66c
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-03-29 23:10:09 +0900

    [Silver II 11279] Title: 최대 힙, Time: 1048 ms, Memory: 29992 KB -NKLCBHub

---
+++ 백준/Silver/#11279 최대 힙/최대 힙.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // 연산의 개수 입력
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2)->{
+			return o1 < o2 ? 1 : -1; // 정렬 기준 재정의
+		});
+
+		for(int i =0; i<N; i++){
+			int tmp = Integer.parseInt(br.readLine());
+			if(tmp == 0){ // 입력값이 0 인경우
+				if(pq.isEmpty()){ // 큐가 비어있을 경우 0, 내용물이 있을 경우 가장 큰 값을 출력 
+					System.out.println("0"); 
+				}else{
+					System.out.println(pq.poll());
+				}
+			}else{
+				pq.add(tmp);
+			}
+		}
+		br.close();
+	}
+}
