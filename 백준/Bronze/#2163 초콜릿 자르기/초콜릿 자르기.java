682e74adff35b6aa692f1f8f62af25cb3dd6ae33
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-04-22 17:22:44 +0900

    [Bronze I 2163] Title: 초콜릿 자르기, Time: 132 ms, Memory: 14200 KB -NKLCBHub

---
+++ 백준/Bronze/#2163 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        cnt = M * N - 1; // 초콜릿 공식
+        System.out.println(cnt);
+
+        br.close();
+    }
+}
