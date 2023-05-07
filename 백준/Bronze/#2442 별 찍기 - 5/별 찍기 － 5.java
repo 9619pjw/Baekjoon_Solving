9246ac1022c6fefaa2b1b03f844bbc9d5e3c87e0
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-05-07 17:00:55 +0900

    [Bronze III 2442] Title: 별 찍기 - 5, Time: 308 ms, Memory: 19944 KB -NKLCBHub

---
+++ 백준/Bronze/#2442 별 찍기 - 5/별 찍기 － 5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		String arr[] = new String[N*2-1];
+
+		for(int i = 0; i < N; i++){
+			for(int j = i ; j < N-1; j++){
+				System.out.print(" ");
+			}
+			for(int j = i*2; j>=0; j--){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		br.close();
+	}
+}
