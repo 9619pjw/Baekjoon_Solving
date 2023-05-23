9fb9bbdfaf513317f987a335e25a67d51166ee09
Park Jin Woo <97871451+9619pjw@users.noreply.github.com>
2023-05-23 22:36:02 +0900

    [Bronze III 5086] Title: 배수와 약수, Time: 116 ms, Memory: 13964 KB -NKLCBHub

---
+++ 백준/Bronze/#5086 배수와 약수/배수와 약수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		while(true){
+			st = new StringTokenizer(br.readLine());
+
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+
+			if(A == 0 && B == 0)
+				break;
+			if(A % B == 0){
+				System.out.println("multiple");
+			}
+			else if (B % A == 0){
+				System.out.println("factor");
+			}
+			else{
+				System.out.println("neither");
+			}
+		}
+		br.close();
+	}
+}
