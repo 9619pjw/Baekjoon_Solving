d8ee05a4972ceff7a5876e929f3c25bfd9c0f041
JokerBear_96 <97871451+9619pjw@users.noreply.github.com>
2024-01-21 22:39:15 +0900

    [Silver IV 4134] Title: 다음 소수, Time: 928 ms, Memory: 101360 KB -NKLCBHub

---
+++ 백준/Silver/#4134 다음 소수/다음 소수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        long n;
+        for(int i=0; i<T; i++){
+            n = Long.parseLong(br.readLine());
+            BigInteger bigInt = new BigInteger(String.valueOf(n));
+            if (bigInt.isProbablePrime(10)) {
+                System.out.println(bigInt);
+            }
+            else {
+                System.out.println(bigInt.nextProbablePrime());
+            }
+        }
+    }
+}
