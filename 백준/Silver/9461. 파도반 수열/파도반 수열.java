2d19428b1bec2c1807968dead6d000fadb7b4d4b
JokerBear <97871451+9619pjw@users.noreply.github.com>
2024-03-18 09:55:32 +0900

    [Silver III] Title: 파도반 수열, Time: 144 ms, Memory: 15748 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < T; i++){
+            int N = Integer.parseInt(br.readLine());
+            long dp[] = new long[N+1];
+
+            dp[0] = 0;
+            dp[1] = 1;
+
+            if(N > 1){
+                dp[2] = 1;
+                for(int j = 3; j <= N; j++){
+                    dp[j] = dp[j-3] + dp[j-2];
+                }
+            }
+            bw.write(dp[N] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
