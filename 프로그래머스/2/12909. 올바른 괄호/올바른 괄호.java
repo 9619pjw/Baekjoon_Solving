274ef7bf9878cbf191669b0e323045a8a3386670
JokerBear <97871451+9619pjw@users.noreply.github.com>
2024-10-07 22:05:26 +0900

    [level 2] Title: 올바른 괄호, Time: 14.64 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        
+        if(s.charAt(0) == ')') return false;
+        
+        Stack<Character> st = new Stack<>();
+        
+        try{
+            for(char c : s.toCharArray()){
+                if(c == '(')
+                    st.push(c);
+                else if (c == ')')
+                    st.pop();
+            }
+        }catch (Exception e){
+            return false;
+        }
+        
+        return st.size() == 0 ? true : false;
+    }
+}
