0c18a9b6bb8d33a561b73a863f21b58f48fc3aee
JokerBear <97871451+9619pjw@users.noreply.github.com>
2026-07-21 10:24:21 +0900

    Create 10진수를 2진수로 변환.java

---
+++ Others/10진수를 2진수로 변환.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        
+        System.out.println(solution(10));
+        System.out.println(solution(27));
+        System.out.println(solution(12345));
+    }
+
+    public static String solution(int dirs) {
+
+        Stack<Integer> st = new Stack<>();
+        
+        while(dirs > 0){
+            st.push(dirs % 2);
+            dirs /= 2;
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        while(!st.isEmpty()){
+            answer.append(st.pop())
+        }
+        
+        return answer;
+    }
+}
